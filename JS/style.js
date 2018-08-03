7f4b307949dc7f7e868ca0b8681e34fe20909248
Pruvot Fabian <pruvotfabian@gmail.com>
2018-08-03 09:40:26 +0200

    first commit

---
+++ JS/style.js
@@ -0,0 +1,7 @@
+var test= document.getElementById("red");
+var t= document.getElementById("blue");
+var te= document.getElementById("orange");
+
+test.style.borderColor= "red";
+t.style.borderColor= "blue";
+te.style.borderColor= "orange";
